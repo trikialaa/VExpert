dd5207d9846d77ea3f343950ec2623d85548e0d2
trikialaa <36111710+trikialaa@users.noreply.github.com>
2019-04-02 02:51:03 +0100

    Added new folder third page + Fixed errors

---
+++ app/src/main/java/com/zettabyte/vexpert/NewFolder4Activity.kt
@@ -0,0 +1,12 @@
+package com.zettabyte.vexpert
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class NewFolder4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_folder4)
+    }
+}
