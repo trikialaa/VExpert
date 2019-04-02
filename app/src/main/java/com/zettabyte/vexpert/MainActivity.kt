cf4f1dad539416a835cbb7eadf0f7a4cc0f8f974
mohameddhaifbelja <mohamedbeljaa@gmail.com>
2019-04-02 03:38:05 +0100

    doing my work

--- app/src/main/java/com/zettabyte/vexpert/MainActivity.kt
+++ app/src/main/java/com/zettabyte/vexpert/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.zettabyte.vexpert
-
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.FirebaseApp
+import kotlinx.android.synthetic.main.activity_login.*
 
 class MainActivity : AppCompatActivity() {
-
+    var login:String=""
+    lateinit var fbAuth:FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
+        FirebaseApp.initializeApp(this)
+        fbAuth = FirebaseAuth.getInstance()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
+        button.setOnClickListener({
+
+            login=username.text.toString()
+            loginWithFirebase(username.text.toString(),password.text.toString())});
+
+    }
+    fun loginWithFirebase(username:String,password:String){
+        fbAuth.createUserWithEmailAndPassword(username+"@auth.com", password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(baseContext, "Authentication failed.",
+                        Toast.LENGTH_SHORT).show()
+
+                }
+
+                // ...
+            }
+        var mintenet= Intent(this,MenuActivity::class.java)
+        mintenet.putExtra("login", login)
+        startActivity(mintenet)
+
     }
 }
--- app/src/main/java/com/zettabyte/vexpert/MenuActivity.kt
+++ app/src/main/java/com/zettabyte/vexpert/MenuActivity.kt
@@ -2,11 +2,42 @@ package com.zettabyte.vexpert
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_menu.*
+
 
 class MenuActivity : AppCompatActivity() {
+    lateinit var login:String
+    lateinit var db: FirebaseDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+        login= intent.getStringExtra("login");
+        db= FirebaseDatabase.getInstance()
+        var ref:DatabaseReference=db.reference.child("experts");
+        // Read from the database
+        ref.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                nom.text = dataSnapshot.child("nom_complet").value.toString();
+
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Failed to read value
+
+            }
+        })
+
+
+
+
     }
 }
